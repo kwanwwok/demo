a6cf1e20affb5fd8407f0166edd87c505eeb07ab
kshom <kshom@DESKTOP-JTMQT3E>
2022-06-10 12:59:10 +0900

    컨트롤러, index수정 2개 추가 by hong

---
+++ demo/src/main/java/com/example/demo/controller/HongController.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+public class HongController {
+	public void hong() {
+		System.out.println("hong!");
+	}
+}
--- demo/src/main/webapp/index.jsp
+++ demo/src/main/webapp/index.jsp
@@ -8,5 +8,6 @@
 </head>
 <body>
 <h1>index</h1>
+<p>by hong</p>
 </body>
 </html>
